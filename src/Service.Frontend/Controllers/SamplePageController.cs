62e3d2299cc747560473e10924b0174bb3393928
SeBastArt <sebastian.schueler1@gmx.de>
2023-02-28 21:36:59 +0100

    initial commit

---
+++ src/Service.Frontend/Controllers/SamplePageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Frontend.Controllers
+{
+    public class SamplePageController : Controller
+    {
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Service.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Service.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
